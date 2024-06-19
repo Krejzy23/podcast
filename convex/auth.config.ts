f97fc53a7f1da664200c1c9f4bf28fe974a7e04e
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-19 17:22:07 +0200

    add clerk sigIn and signUp functionality and UI

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://allowed-ant-7.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
