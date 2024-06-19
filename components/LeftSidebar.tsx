2b4bea9697e3c6d128143384acab23458856016c
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-19 11:49:07 +0200

    add leftSidebar,rightSider and MobNav components

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { sidebarLinks } from '@/constants'
+import { usePathname, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <section className='left_sidebar'>
       <nav className='flex flex-col gap-6'>
@@ -11,9 +17,14 @@ const LeftSidebar = () => {
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27}/>
           <h1 className='text-24 font-extrabold text-white-1 max-lg:hidden'>Podcastr</h1>
         </Link>
-        {sidebarLinks.map(({ route, label }) => {
-          return <Link href={route}>
-            {label}
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive = pathname === route || pathname.startsWith(`${route}/`);
+
+          return <Link href={route} key={label} className={cn("flex items-center gap-3 py-4 lg:justify-start max-lg:px-4", {
+            'bg-nav-focus border-r-4 border-orange-1' : isActive
+          })}>
+            <Image src={imgURL} alt={label} width={24} height={24}/>
+            <p>{label}</p>
           </Link>
         })}
       </nav>
