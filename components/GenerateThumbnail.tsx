077ea0abd5ffe51ef2be536d48615dd1044f78e7
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-20 13:10:23 +0200

    add thumbnail button funcionality and UI/UIX

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { GenerateThumbnailProps } from "@/types";
+import { Loader } from "lucide-react";
 
-const GenerateThumbnail = () => {
+const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt }: GenerateThumbnailProps) => {
+
+  const [isAiThumbnail, setIsAiThumbnail] = useState(false);
+
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const generateImage = async() => {}
+
   return (
-    <div>GenerateThumbnail</div>
-  )
-}
+    <>
+      <div className="generate_thumbnail">
+        <Button
+          type="button"
+          variant="plain"
+          className={cn("", {
+            "bg-black-6": isAiThumbnail,
+          })}
+          onClick={() => setIsAiThumbnail(true)}
+        >
+          Use AI to generate thumbnail
+        </Button>
+        <Button
+          type="button"
+          variant="plain"
+          className={cn("", {
+            "bg-black-6": !isAiThumbnail,
+          })}
+          onClick={() => setIsAiThumbnail(false)}
+        >
+          Upload custom image
+        </Button>
+      </div>
+      {isAiThumbnail ? (
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col mt-5 gap-2.5">
+            <Label className="text-16 font-bold text-white-1">
+              AI Prompt to generate Thumbnail
+            </Label>
+            <Textarea
+              className="input-class font-light focus-visible:ring-offset-orange-1"
+              placeholder="Provide text to generate thumbnail"
+              rows={5}
+              value={imagePrompt}
+              onChange={(e) => setImagePrompt(e.target.value)}
+            />
+          </div>
 
-export default GenerateThumbnail
+          <div className="w-full max-w-[200px]">
+            <Button
+              type="submit"
+              className="text-16 bg-orange-1 py-4 font-bold text-white-1"
+              onClick={generateImage}
+            >
+              {isGenerating ? (
+                <>
+                  <Loader size={20} className="animate-spin ml-2" />
+                </>
+              ) : (
+                "Generate"
+              )}
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
+};
+
+export default GenerateThumbnail;
