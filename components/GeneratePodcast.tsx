2b68d35f07b0ee8e99a438f6ac736c756844fc84
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-20 07:54:30 +0200

    UI/UIX generate podcast page and some tightennig

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,17 +1,21 @@
 import { GeneratePodcastProps } from '@/types'
-import React from 'react'
+import React, { useState } from 'react'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+import { Loader } from 'lucide-react'
 
-const GeneratePodcast = ({
-    setAudioStorageId,
-    setAudio,
-    voiceType,
-    audio,
-    voicePrompt,
-    setVoicePrompt,
-    setAudioDuration,
-} : GeneratePodcastProps ) => {
+const useGeneratePodcast = (props: GeneratePodcastProps) => {
+
+  return {
+    isGenerating: false,
+    generatePodcast: () => {}
+  }
+}
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+  const {isGenerating, generatePodcast} = useGeneratePodcast(props);
+
   return (
     <div>
       <div className='flex flex-col gap-2.5'>
@@ -22,10 +26,31 @@ const GeneratePodcast = ({
           className='input-class font-light focus-visible:ring-offset-orange-1'
           placeholder='Provide text to generate audio'
           rows={5}
-          value={voicePrompt}
-          onChange={(e) => setVoicePrompt(e.target.value)}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
         />
       </div>
+      
+      <div className='mt-5 w-full max-w-[200px]'>
+        <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generatePodcast}>
+          {isGenerating ? (
+            <>
+              <Loader size={20} className="animate-spin ml-2"/>
+            </>
+          ):(
+            'Generate'
+          )}
+        </Button>
+      </div>
+      {props.audio && (
+        <audio 
+          controls
+          src={props.audio}
+          autoPlay
+          className='mt-5'
+          onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
+        />
+      )}
     </div>
   )
 }
