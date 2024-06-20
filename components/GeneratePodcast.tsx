69fcfbdc3b836132790de979bf0535b18de4f752
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-20 10:48:28 +0200

    add uuid an podcasts page on storage Url on Convex

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -4,13 +4,20 @@ import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { Loader } from 'lucide-react'
-import { useAction } from 'convex/react'
+import { useAction, useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
+import { v4 as uuidv4 } from "uuid"
+import { generateUploadUrl } from '@/convex/files'
+import { useUploadFiles } from '@xixixao/uploadstuff/react';
 
 const useGeneratePodcast = ({
   setAudio, voiceType, voicePrompt, setAudioStorageId
 } : GeneratePodcastProps) => {
   const [isGenerating, setIsGenerating] = useState(false);
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  
+  const { startUpload } = useUploadFiles(generateUploadUrl)
 
   const getPodcastAudio = useAction(api.openai.generateAudioAction)
 
@@ -28,8 +35,20 @@ const useGeneratePodcast = ({
         voice: voiceType,
         input: voicePrompt,
       });
-      // Předpokládám, že tady nastavíš audio pomocí setAudio nebo setAudioStorageId
+
+      const blob = new Blob([response], { type: 'audio/mpeg' });
+      const fileName = `podcast-${uuidv4()}.mp3`
+      const file = new File([blob], fileName, { type: 'audio/mpeg' });
+
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setAudioStorageId(storageId);
+
+      const audioUrl = await getAudioUrl({ storageId });
+      setAudio(audioUrl!);
       setIsGenerating(false);
+      
     } catch (error) {
       console.log('Error generating podcast', error);
       setIsGenerating(false);
