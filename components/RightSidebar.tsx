50de76b0f7c8262597f7603147892dacd2e1f821
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-21 09:45:00 +0200

    add header component in rightSidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,11 @@
 'use client'
 
-import { SignedIn, useUser } from '@clerk/nextjs';
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import Header from './Header';
+import Carousel from './Carousel';
 
 const RightSidebar = () => {
 
@@ -10,8 +14,23 @@ const RightSidebar = () => {
   return (
     <section className='right_sidebar text-white-1'>
       <SignedIn>
-        <h1 className="">{user?.firstName}</h1>
+        <Link href={`/profile/${user?.id}`} className='flex gap-3 pb-12'>
+          <UserButton />
+          <div className='flex w-full items-center justify-between'>
+            <h1 className="text-16 truncate font-semibold text-white-1">{user?.firstName} {user?.lastName}</h1>
+            <Image 
+              src="/icons/right-arrow.svg"
+              alt='arrow'
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
       </SignedIn>
+      <section className="">
+        <Header headerTitle="Fans like You"/>
+        <Carousel />
+      </section>
     </section>
   )
 }
