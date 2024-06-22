ca9350a0cb32103fba8c412a30e6185a2fbd2e1a
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-22 10:15:17 +0200

    add podcastr player,progress bar

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import EmptyState from "@/components/EmptyState";
 import LoaderSpinner from "@/components/LoaderSpinner";
@@ -14,17 +14,19 @@ import React from "react";
 const PodcastDetails = ({
   params: { podcastId },
 }: {
-  params: { podcastId: Id<'podcasts'> };
+  params: { podcastId: Id<"podcasts"> };
 }) => {
   const { user } = useUser();
 
   const podcast = useQuery(api.podcasts.getPodcastById, { podcastId });
 
-  const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, { podcastId });
+  const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, {
+    podcastId,
+  });
 
   const isOwner = user?.id === podcast?.authorId;
 
-  if(!similarPodcasts || !podcast) return <LoaderSpinner />
+  if (!similarPodcasts || !podcast) return <LoaderSpinner />;
 
   return (
     <section className="flex w-full flex-col">
@@ -37,27 +39,31 @@ const PodcastDetails = ({
             height={24}
             alt="headphone"
           />
-          <h2 className="text-16 font-bold text-white-1">
-            {podcast?.views}
-          </h2>
+          <h2 className="text-16 font-bold text-white-1">{podcast?.views}</h2>
         </figure>
       </header>
-      <PodcastDetailPlayer 
+      <PodcastDetailPlayer
         isOwner={isOwner}
         podcastId={podcast._id}
         {...podcast}
       />
 
-      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">{podcast?.podcastDescription}</p>
+      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">
+        {podcast?.podcastDescription}
+      </p>
 
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-4">
           <h1 className="text-18 font-bold text-white-1">Transcription</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.voicePrompt}</p>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.voicePrompt}
+          </p>
         </div>
         <div className="flex flex-col gap-4">
           <h1 className="text-18 font-bold text-white-1">Thumbnail Prompt</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.imagePrompt}</p>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.imagePrompt}
+          </p>
         </div>
       </div>
       <section className="flex flex-col gap-5 mt-8">
@@ -65,19 +71,21 @@ const PodcastDetails = ({
 
         {similarPodcasts && similarPodcasts.length > 0 ? (
           <div className="podcast_grid">
-            {similarPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-              <PodcastCard 
-                key={_id}
-                imgUrl={imageUrl as string}
-                title={podcastTitle}
-                description={podcastDescription}
-                podcastId={_id}
-              />
-            ))}
+            {similarPodcasts?.map(
+              ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                <PodcastCard
+                  key={_id}
+                  imgUrl={imageUrl as string}
+                  title={podcastTitle}
+                  description={podcastDescription}
+                  podcastId={_id}
+                />
+              )
+            )}
           </div>
         ) : (
-          <> 
-            <EmptyState 
+          <>
+            <EmptyState
               title="No similar podcasts found"
               buttonLink="/discover"
               buttonText="Discover more podcasts"
--- components/PodcastDetailPlayer.tsx
+++ components/PodcastDetailPlayer.tsx
@@ -10,7 +10,8 @@ import { PodcastDetailPlayerProps } from "@/types";
 import LoaderSpinner from "./LoaderSpinner";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
-// import { useAudio } from "@/providers/AudioProvider";
+import { useAudio } from "@/providers/AudioProvider";
+
 
 const PodcastDetailPlayer = ({
   audioUrl,
@@ -23,9 +24,9 @@ const PodcastDetailPlayer = ({
   isOwner,
   authorImageUrl,
   authorId,
-}: PodcastDetailPlayerProps) => {
+} : PodcastDetailPlayerProps) => {
   const router = useRouter();
-//   const { setAudio } = useAudio();
+  const { setAudio } = useAudio();
   const { toast } = useToast();
   const [isDeleting, setIsDeleting] = useState(false);
   const deletePodcast = useMutation(api.podcasts.deletePodcast);
@@ -47,13 +48,13 @@ const PodcastDetailPlayer = ({
   };
 
   const handlePlay = () => {
-    // setAudio({
-    //   title: podcastTitle,
-    //   audioUrl,
-    //   imageUrl,
-    //   author,
-    //   podcastId,
-    // });
+    setAudio({
+      title: podcastTitle,
+      audioUrl,
+      imageUrl,
+      author,
+      podcastId,
+    });
   };
 
   if (!imageUrl || !authorImageUrl) return <LoaderSpinner />;
