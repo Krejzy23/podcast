c6b85b36f91f3f3e484cf66669f16023da1c1459
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-22 22:51:23 +0200

    add log out button on mob nav

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,55 +1,71 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import { sidebarLinks } from '@/constants'
-import { usePathname, useRouter } from 'next/navigation'
-import { cn } from '@/lib/utils'
-import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs';
-import { Button } from './ui/button';
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { signOut } = useClerk();
   return (
-    <section className='left_sidebar'>
-      <nav className='flex flex-col gap-6'>
-        <Link href="/" className='flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'>
-          <Image src="/icons/logo.svg" alt="logo" width={23} height={27}/>
-          <h1 className='text-24 font-extrabold text-white-1 max-lg:hidden'>Podcastr</h1>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
+            Podcastr
+          </h1>
         </Link>
         {sidebarLinks.map(({ route, label, imgURL }) => {
-          const isActive = pathname === route || pathname.startsWith(`${route}/`);
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
 
-          return <Link href={route} key={label} className={cn("flex items-center gap-3 py-4 lg:justify-start max-lg:px-4", {
-            'bg-nav-focus border-r-4 border-orange-1' : isActive
-          })}>
-            <Image src={imgURL} alt={label} width={24} height={24}/>
-            <p>{label}</p>
-          </Link>
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex items-center gap-3 py-4 lg:justify-start max-lg:px-4",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
         })}
       </nav>
       <SignedOut>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button  
-            className="text-16 w-full bg-orange-1 font-extrabold" >
-              Sign in
+          <Button className="text-16 w-full bg-orange-1 font-extrabold">
+            Sign in
           </Button>
         </div>
       </SignedOut>
       <SignedIn>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button asChild className="text-16 w-full bg-orange-1 font-extrabold" onClick={() => signOut(() =>router.push('/'))}>
-            <Link href="/sign-in">
-              Log out
-            </Link>
+          <Button
+            asChild
+            className="text-16 w-full bg-orange-1 font-extrabold"
+            onClick={() => signOut(() => router.push("/"))}
+          >
+            <Link href="/sign-in">Log out</Link>
           </Button>
         </div>
       </SignedIn>
     </section>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -8,12 +8,17 @@ import {
 } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import router from "next/router";
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const router = useRouter();
+  const { signOut } = useClerk();
 
   return (
     <section>
@@ -38,35 +43,55 @@ const MobileNav = () => {
             </h1>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
-            <SheetClose asChild>
-              <nav className="flex h-full flex-col gap-6 text-white-1">
-                {sidebarLinks.map(({ route, label, imgURL }) => {
-                  const isActive =
-                    pathname === route || pathname.startsWith(`${route}/`);
+            <nav className="flex h-full flex-col gap-6 text-white-1">
+              {sidebarLinks.map(({ route, label, imgURL }) => {
+                const isActive =
+                  pathname === route || pathname.startsWith(`${route}/`);
 
-                  return (
-                    <SheetClose asChild key={route}>
-                      <Link
-                        href={route}
-                        className={cn(
-                          "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
-                          {
-                            "bg-nav-focus border-r-4 border-orange-1": isActive,
-                          }
-                        )}
-                      >
-                        <Image
-                          src={imgURL}
-                          alt={label}
-                          width={24}
-                          height={24}
-                        />
-                        <p>{label}</p>
-                      </Link>
-                    </SheetClose>
-                  );
-                })}
-              </nav>
+                return (
+                  <SheetClose asChild key={route}>
+                    <Link
+                      href={route}
+                      className={cn(
+                        "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
+                        {
+                          "bg-nav-focus border-r-4 border-orange-1": isActive,
+                        }
+                      )}
+                    >
+                      <Image
+                        src={imgURL}
+                        alt={label}
+                        width={24}
+                        height={24}
+                      />
+                      <p>{label}</p>
+                    </Link>
+                  </SheetClose>
+                );
+              })}
+            </nav>
+            <SheetClose asChild>
+              <SignedOut>
+                <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                  <Button className="text-16 w-full bg-orange-1 font-extrabold">
+                    Sign in
+                  </Button>
+                </div>
+              </SignedOut>
+            </SheetClose>
+            <SheetClose asChild>
+              <SignedIn>
+                <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                  <Button
+                    asChild
+                    className="text-16 w-full bg-orange-1 font-extrabold"
+                    onClick={() => signOut(() => router.push("/"))}
+                  >
+                    <Link href="/sign-in">Log out</Link>
+                  </Button>
+                </div>
+              </SignedIn>
             </SheetClose>
           </div>
         </SheetContent>
